c50ac91a7a7bde0face24c26e73bd84b29a0a4fb
vins06 <43523396+vins06@users.noreply.github.com>
2018-09-23 22:23:24 +0300

    Create Zoo_All_MN_10_Masternode status.sh

---
+++ Zoo_All_MN_10_Masternode status.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo
+
+echo "Masternode status zoo01"
+
+./zood -datadir=/root/.zoo01 masternode status
+
+echo
+
+echo "Masternode status zoo02"
+
+./zood -datadir=/root/.zoo02 masternode status
+
+echo
+
+echo "Masternode status zoo03"
+
+./zood -datadir=/root/.zoo03 masternode status
+
+echo
+
+echo "Masternode status zoo04"
+
+./zood -datadir=/root/.zoo04 masternode status
+
+echo
+
+echo "Masternode status zoo05"
+
+./zood -datadir=/root/.zoo05 masternode status
+
+echo
+
+echo "Masternode status zoo06"
+
+./zood -datadir=/root/.zoo06 masternode status
+
+echo
+
+echo "Masternode status zoo07"
+
+./zood -datadir=/root/.zoo07 masternode status
+
+echo
+
+echo "Masternode status zoo08"
+
+./zood -datadir=/root/.zoo08 masternode status
+
+echo
+
+echo "Masternode status zoo09"
+
+./zood -datadir=/root/.zoo09 masternode status
+
+echo
+
+echo "Masternode status zoo10"
+
+./zood -datadir=/root/.zoo10 masternode status
+
+echo
+
+echo "Done!!!"
