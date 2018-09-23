71418559cfd1f901712f2011520a4a27e5faebf3
vins06 <43523396+vins06@users.noreply.github.com>
2018-09-23 23:35:48 +0300

    Update Zoo_All_MN_10_Masternode_status.sh

--- Zoo_All_MN_10_Masternode_status.sh
+++ Zoo_All_MN_10_Masternode_status.sh
@@ -2,9 +2,65 @@
 
 echo
 
-echo "Masternode status zoo01"
 cd /root
+
+echo "Masternode status zoo01"
+
 ./zood -datadir=/root/.zoo01 masternode status
+
+echo
+
+echo "Masternode status zoo02"
+
+./zood -datadir=/root/.zoo02 masternode status
+
+echo
+
+echo "Masternode status zoo03"
+
+./zood -datadir=/root/.zoo03 masternode status
+
+echo
+
+echo "Masternode status zoo04"
+
+./zood -datadir=/root/.zoo04 masternode status
+
+echo
+
+echo "Masternode status zoo05"
+
+./zood -datadir=/root/.zoo05 masternode status
+
+echo
+
+echo "Masternode status zoo06"
+
+./zood -datadir=/root/.zoo06 masternode status
+
+echo
+
+echo "Masternode status zoo07"
+
+./zood -datadir=/root/.zoo07 masternode status
+
+echo
+
+echo "Masternode status zoo08"
+
+./zood -datadir=/root/.zoo08 masternode status
+
+echo
+
+echo "Masternode status zoo09"
+
+./zood -datadir=/root/.zoo09 masternode status
+
+echo
+
+echo "Masternode status zoo10"
+
+./zood -datadir=/root/.zoo10 masternode status
 
 echo
 
